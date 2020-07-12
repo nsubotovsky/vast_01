ea42a472461c32b407d375e64a7831900cb0ecb1
nsubotovsky <54336861+nsubotovsky@users.noreply.github.com>
2020-07-11 21:33:02 -0300

    preprocess stuff

--- preprocess.r
+++ preprocess.r
@@ -8,80 +8,162 @@ library(plyr)
 library(readr)
 library(data.table)
 library(lubridate)
-#library(hash)
 library(tidyverse)
 library(purrr)
-
-
-graph_path <- "D:/Nicolas/Estudios/Maestria/3. [Sab] Visualizacion/TP Vast MC1/data/Q1-Graph1.csv"
-
-nodetype_ref <- "D:/Nicolas/Estudios/Maestria/3. [Sab] Visualizacion/TP Vast MC1/data/full graph/CGCS-GraphData-NodeTypes.csv"
+library(glue)
 
 
 
 
-factor_edgetypes <- function( col_edgeTypes )
+############ graph paths ##############
+#######################################
+
+
+graph.get.path.candidate <- function( n )
 {
-    return( col_edgeTypes %>%
-                mapvalues( from = c(0,1,2,3,4,5,6), to = c("0_emails", "1_phone", "2_sell", "3_buy", "4_author", "5_financial", "6_travels")) %>%
-                as.factor() )
+    return( file.path( getwd(), "data", "Q1-Graph%s.csv" %>% sprintf( n ) ) )
+}
+
+graph.get.path.template <- function()
+{
+    return( file.path( getwd(), "data", "template", "CGCS-Template.csv" ) )
+}
+
+graph.get.path.full <- function()
+{
+    return( file.path( getwd(), "data", "full graph", "CGCS-GraphData.csv" ) )
 }
 
 
-factor_nodetypes <- function( col_nodeTypes )
+########### refdata paths #############
+#######################################
+
+
+refdata.get.path.template <- function()
 {
-    return( col_nodeTypes %>%
-                mapvalues( from = c(1,2,3,4,5), to = c("1_person", "2_product", "3_document", "4_financial", "5_country")) %>%
-                as.factor() )
+    return( file.path( getwd(), "data", "template", "CGCS-Template.csv" ) )
+}
+
+refdata.get.path.full <- function()
+{
+    return( file.path( getwd(), "data", "full graph", "CGCS-GraphData-NodeTypes.csv" ) )
+}
+
+refdata.get.path.demographics <- function()
+{
+    return( file.path( getwd(), "data", "refdata", "DemographicCategories.csv" ) )
+}
+
+
+############ Graph direct processing #############
+##################################################
+
+graph.load <- function( path )
+{
+    graph <- read_csv(path, 
+                      col_types = cols(Source = col_integer(), 
+                                       Target = col_integer(),
+                                       Time = col_integer(), 
+                                       eType = col_integer())
+                      ) %>% as.data.table()
+}
+
+
+graph.process.dates <- function( dt )
+{
+    return( dt %>% mutate( Time=as.Date(as_datetime(Time + as.numeric(as.POSIXct("2025-01-01 00:00:00 GMT"))) )) );
+}
+
+graph.process.edges <- function( dt )
+{
+    return( 
+        dt %>%
+        mutate(
+            eType=mapvalues(eType,
+                            from = c(0,1,2,3,4,5,6),
+                            to = c("0_emails", "1_phone", "2_sell", "3_buy", "4_author", "5_financial", "6_travels")
+            ) %>%
+        as.factor() )
+    );
+    
+}
+
+
+graph.process.prefixTypes <- function( dt, refdata )
+{
+    enrich_id <- function( idNum, idType )
+    {
+        strId <- toString(idNum)
+        if ( idType == "1_person" ) return( paste0( "pers_", strId ) )
+        if ( idType == "2_product" ) return( paste0( "prod_", strId ) )
+        if ( idType == "3_document" ) return( paste0( "doc_", strId ) )
+        if ( idType == "4_financial" ) return( paste0( "fin_", strId ) )
+        if ( idType == "5_country" ) return( paste0( "count_", strId ) )
+        return( paste0( "unkn_", strId ))
+    }
+    
+    
+    dt <- dt %>% left_join( ( refdata %>% dplyr::rename(Source=NodeID) %>% dplyr::rename(SourceType=NodeType) ), by='Source' )
+    dt <- dt %>% mutate( Source=map2_chr(Source, SourceType, enrich_id) ) %>% select(-SourceType )
+    
+    
+    dt <- dt %>% left_join( ( refdata %>% dplyr::rename(Target=NodeID) %>% dplyr::rename(TargetType=NodeType) ), by='Target' )
+    dt <- dt %>% mutate( Target=map2_chr(Target, TargetType, enrich_id) ) %>% select(-TargetType )
+    
+    return( dt );
 }
 
 
 
+refdata.process.types <- function( dt )
+{
+    return( dt %>%
+            mutate(
+                NodeType=mapvalues(NodeType,
+                                   from = c(1,2,3,4,5),
+                                   to = c("1_person", "2_product", "3_document", "4_financial", "5_country")
+           ) %>%
+        as.factor() )
+    );
+}
 
 
-graph <- read_csv(graph_path, 
-                  col_types = cols(Source = col_integer(), 
-                                   Target = col_integer(), Time = col_integer(), 
-                                   eType = col_integer())) %>% as.data.table()
 
-
-# Fix time with offset
-base_date <- as.numeric(as.POSIXct("2025-01-01 2:13:46 GMT"))
-graph <- graph %>% mutate( Time=as.Date(as_datetime(Time + base_date) ))
-
-
-# convert eType to factor
-graph$eType <- graph$eType %>% factor_edgetypes()
-
-
-nodetypes <- read_csv(nodetype_ref, 
+refdata.nodetypes.load <- function( path )
+{
+    graph <- read_csv(path, 
                       col_types = cols(NodeID = col_integer(), 
-                                       NodeType = col_integer())) %>% as.data.table()
-
-nodetypes$NodeType <- nodetypes$NodeType %>% factor_nodetypes()
-
-
-graph <- left_join( graph, ( nodetypes %>% dplyr::rename(Source=NodeID) %>% dplyr::rename(SourceType=NodeType) ), by='Source' )
-
-graph <- left_join( graph, ( nodetypes %>% dplyr::rename(Target=NodeID) %>% dplyr::rename(TargetType=NodeType) ), by='Target' )
-
-
-enrich_id <- function( idNum, idType )
-{
-    strId <- toString(idNum)
-    if ( idType == "1_person" ) return( paste0( "pers_", strId ) )
-    if ( idType == "2_product" ) return( paste0( "prod_", strId ) )
-    if ( idType == "3_document" ) return( paste0( "doc_", strId ) )
-    if ( idType == "4_financial" ) return( paste0( "fin_", strId ) )
-    if ( idType == "5_country" ) return( paste0( "count_", strId ) )
-    return( paste0( "unkn_", strId ))
+                                       NodeType = col_integer())
+    ) %>% as.data.table()
 }
 
 
-graph <- graph %>% mutate( Source=map2_chr(Source, SourceType, enrich_id) ) %>% select(-SourceType )
 
 
-graph <- graph %>% mutate( Target=map2_chr(Target, TargetType, enrich_id) ) %>% select(-TargetType )
+
+
+
+
+nodetypes <- refdata.get.path.full() %>%
+    refdata.nodetypes.load() %>%
+    refdata.process.types()
+
+
+
+
+g2 <- graph.get.path.candidate( 1 ) %>%
+    graph.load() %>%
+    graph.process.edges() %>%
+    graph.process.dates() %>%
+
+    graph.process.prefixTypes( nodetypes )
+
+
+
+
+
+##########################################
+
 
 
 aaa<- graph[complete.cases(graph),]
@@ -95,42 +177,4 @@ b <- graph %>% filter(  eType == "6_travels" ) %>% select( Source, SourceLocatio
 
 
 ####################
-
-
-h <- hash(keys=nodetypes$NodeID, values=nodetypes$NodeType)
-
-
-
-graph %>% mutate( aSource=h[[toString(Source)]] ) %>% head()
-
-
-map( graph[['Source']], function(id) { return( h[[toString(id)]] ); } )
-
-
-
-toNamed <- function( nodeId, nodeTable )
-{
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-graph <- graph %>% filter( eType==6  )
-
-
-
-
-View(graph)
-
-
 
